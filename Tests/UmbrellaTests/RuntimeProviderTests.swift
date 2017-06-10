8e83dda7539e2adfc3fd8e602832aeedacdac3a3
Suyeol Jeon <devxoul@gmail.com>
2017-06-11 07:13:35 +0900

    Add test for method RuntimeProviderType's method invocation

--- Tests/UmbrellaTests/RuntimeProviderTests.swift
+++ Tests/UmbrellaTests/RuntimeProviderTests.swift
@@ -2,7 +2,10 @@ import XCTest
 import Umbrella
 
 final class ClassAnalytics: NSObject {
+  static var loggedEvents: [(name: String, parameters: [String: Any]?)] = []
+
   class func logEvent(_ eventName: String, parameters: [String: Any]?) {
+    self.loggedEvents.append((name: eventName, parameters: parameters))
   }
 }
 
@@ -13,7 +16,10 @@ final class ClassProvider: RuntimeProviderType {
 
 final class InstanceAnalytics: NSObject {
   static let shared = InstanceAnalytics()
-  func logEvent(_ eventName: String, parameters: [String: Any]?) {
+  var loggedEvents: [(name: String, parameters: [String: Any]?)] = []
+
+  @objc func logEvent(_ eventName: String, parameters: [String: Any]?) {
+    self.loggedEvents.append((name: eventName, parameters: parameters))
   }
 }
 
@@ -24,18 +30,33 @@ final class InstanceProvider: RuntimeProviderType {
 }
 
 final class RuntimeProviderTests: XCTestCase {
+  override func setUp() {
+    super.setUp()
+    ClassAnalytics.loggedEvents.removeAll()
+    InstanceAnalytics.shared.loggedEvents.removeAll()
+  }
+
   func testClassProvider() {
     let provider = ClassProvider()
+    provider.log("purchase", parameters: ["product_id": 123, "price": 9.99])
     XCTAssertTrue(provider.cls === ClassAnalytics.self)
     XCTAssertEqual(provider.selector, #selector(ClassAnalytics.logEvent(_:parameters:)))
     XCTAssertTrue(provider.responds)
+    XCTAssertEqual(ClassAnalytics.loggedEvents.count, 1)
+    XCTAssertEqual(ClassAnalytics.loggedEvents[0].name, "purchase")
+    XCTAssertEqual(ClassAnalytics.loggedEvents[0].parameters!["product_id"] as! Int, 123)
+    XCTAssertEqual(ClassAnalytics.loggedEvents[0].parameters!["price"] as! Double, 9.99)
   }
 
   func testInstanceProvider() {
     let provider = InstanceProvider()
+    provider.log("purchase", parameters: ["product_id": 123, "price": 9.99])
     XCTAssertTrue(provider.cls === InstanceAnalytics.self)
     XCTAssertTrue(provider.instance === InstanceAnalytics.shared)
     XCTAssertEqual(provider.selector, #selector(InstanceAnalytics.logEvent(_:parameters:)))
     XCTAssertTrue(provider.responds)
+    XCTAssertEqual(InstanceAnalytics.shared.loggedEvents[0].name, "purchase")
+    XCTAssertEqual(InstanceAnalytics.shared.loggedEvents[0].parameters!["product_id"] as! Int, 123)
+    XCTAssertEqual(InstanceAnalytics.shared.loggedEvents[0].parameters!["price"] as! Double, 9.99)
   }
 }
