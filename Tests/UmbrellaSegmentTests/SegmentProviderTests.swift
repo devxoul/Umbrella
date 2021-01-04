144b9bad16408536977a2efc2285b8ab1d893147
James Greenhill <fuziontech@gmail.com>
2021-01-03 22:10:00 -0800

    Tune up some tests

--- Tests/UmbrellaSegmentTests/SegmentProviderTests.swift
+++ Tests/UmbrellaSegmentTests/SegmentProviderTests.swift
@@ -6,16 +6,16 @@ import Analytics
 final class SegmentProviderTests: XCTestCase {
   override func setUp() {
     super.setUp()
-    let configuration = SEGAnalyticsConfiguration(writeKey: "YOUR_WRITE_KEY")
-    SEGAnalytics.setup(with: configuration)
+    let configuration = AnalyticsConfiguration(writeKey: "YOUR_WRITE_KEY")
+    Analytics.setup(with: configuration)
   }
 
   func testSegmentProvider() {
     let provider = SegmentProvider()
-    XCTAssertTrue(provider.cls === SEGAnalytics.self)
+    XCTAssertTrue(provider.cls === Analytics.self)
     XCTAssertNotNil(provider.instance)
-    XCTAssertTrue(provider.instance === SEGAnalytics.shared())
-    XCTAssertEqual(provider.selector, #selector(SEGAnalytics.track(_:properties:)))
+    XCTAssertTrue(provider.instance === Analytics.shared())
+    XCTAssertEqual(provider.selector, #selector(Analytics.track(_:properties:)))
     XCTAssertTrue(provider.responds)
   }
 }
